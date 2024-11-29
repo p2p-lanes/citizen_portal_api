476db2a591fa9718a0c5d3c7888aa3b61c761995
Francisco Viñas <fvinas@fi.uba.ar>
2024-11-29 18:16:57 -0300

    feat: send mail in webhook

--- app/api/webhooks/routes.py
+++ app/api/webhooks/routes.py
@@ -3,17 +3,43 @@ from sqlalchemy.orm import Session
 
 from app.api.webhooks import schemas
 from app.core.database import get_db
+from app.core.mail import send_mail
+from app.core.logger import logger
 
 router = APIRouter()
 
 
 @router.post('/send_email', status_code=status.HTTP_201_CREATED)
-def send_email(
+def send_email_webhook(
     webhook_payload: schemas.WebhookPayload,
     template: str = Query(..., description='Email template name'),
     fields: str = Query(..., description='Template fields'),
     pop_up_city: str = Query(..., description='Pop-up city name'),
     db: Session = Depends(get_db),
 ):
-    # TODO: Implement email sending logic
+    if not webhook_payload.data.rows:
+        logger.info('No rows to send email')
+        return {'message': 'No rows to send email'}
+
+    fields = [f.strip() for f in fields.split(',')]
+    logger.info('Sending email %s to %s rows', template, len(webhook_payload.data.rows))
+    logger.info('Fields: %s', fields)
+    for row in webhook_payload.data.rows:
+        row = row.model_dump()
+        if not row.get('email'):
+            logger.info('No email to send email. Skipping...')
+            continue
+
+        if 'muvinai.com' not in row['email']:
+            logger.info('Email %s is not from Muvinai. Skipping...', row['email'])
+            continue
+
+        params = {k: v for k, v in row.items() if k in fields}
+
+        send_mail(
+            receiver_mail=row['email'],
+            template=template,
+            params=params,
+        )
+
     return {'message': 'Email sent successfully'}
