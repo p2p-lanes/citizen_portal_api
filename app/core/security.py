a0354292b003cdd9d1ff6a17a17ee90859bc1dc9
Francisco Viñas <fvinas@fi.uba.ar>
2024-12-03 15:58:55 -0300

    change: token expiration is optional

--- app/core/security.py
+++ app/core/security.py
@@ -28,11 +28,9 @@ class TokenData(BaseModel):
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
-    if not expires_delta:
-        expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    expire = datetime.utcnow() + expires_delta
+    if expires_delta:
+        to_encode.update({'exp': datetime.utcnow() + expires_delta})
 
-    to_encode.update({'exp': expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
