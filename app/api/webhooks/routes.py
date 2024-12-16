2a9269635dcb21e478649b9c9db70dac23ed81fb
Francisco Viñas <fvinas@fi.uba.ar>
2024-12-16 12:47:58 -0300

    fix: sent_mails when is an empty string

--- app/api/webhooks/routes.py
+++ app/api/webhooks/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, Query, status
-from sqlalchemy import case, literal_column
+from sqlalchemy import case, or_
 from sqlalchemy.orm import Session
 
 from app.api.applications.models import Application
@@ -54,8 +54,14 @@ async def send_email_webhook(
             db.query(Application).filter(Application.id.in_(processed_ids)).update(
                 {
                     Application.sent_mails: case(
-                        (Application.sent_mails.is_(None), template),
-                        else_=Application.sent_mails + ',' + template
+                        (
+                            or_(
+                                Application.sent_mails.is_(None),
+                                Application.sent_mails == '',
+                            ),
+                            template,
+                        ),
+                        else_=Application.sent_mails + ',' + template,
                     )
                 },
                 synchronize_session=False,
