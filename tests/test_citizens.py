00184ac06946b15a3875f3d3a5c43b52720a0f69
Francisco Viñas <fvinas@fi.uba.ar>
2025-01-07 17:14:01 -0300

    test: add login failure and get citizens tests

--- tests/test_citizens.py
+++ tests/test_citizens.py
@@ -1,6 +1,8 @@
 import pytest
 from fastapi import status
 
+from tests.conftest import get_auth_headers_for_citizen
+
 
 def test_signup_success(client):
     response = client.post(
@@ -65,3 +67,44 @@ def test_login_success(client, db_session):
     data = response.json()
     assert data['access_token'] is not None
     assert data['token_type'] == 'Bearer'
+
+
+def test_login_failure(client, db_session):
+    # First create a citizen with known spice
+    from app.api.citizens.crud import citizen
+    from app.api.citizens.schemas import InternalCitizenCreate
+
+    valid_spice = 'testspice123'
+    invalid_spice = 'invalidspice123'
+    new_citizen = InternalCitizenCreate(
+        primary_email='test@example.com', spice=valid_spice
+    )
+    citizen.create(db_session, new_citizen)
+
+    response = client.post(
+        '/citizens/login', params={'email': 'test@example.com', 'spice': invalid_spice}
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json()['detail'] == 'Invalid spice'
+
+    # Invalid email
+    response = client.post(
+        '/citizens/login', params={'email': 'invalid@example.com', 'spice': valid_spice}
+    )
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()['detail'] == 'Citizen not found'
+
+
+def test_get_citizens_success(client, auth_headers):
+    response = client.get('/citizens', headers=auth_headers)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert len(data) == 1
+    assert data[0]['id'] == 1
+
+    headers = get_auth_headers_for_citizen(999)
+
+    response = client.get('/citizens', headers=headers)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert len(data) == 0
