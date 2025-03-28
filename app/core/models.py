6fb51fccdfea0af4efe928a07717edd7b034c401
Francisco Viñas <fvinas@fi.uba.ar>
2025-03-28 10:33:58 -0300

    feat: Add Group model import to core models

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@
 from app.api.applications.models import Application
 from app.api.attendees.models import Attendee
 from app.api.citizens.models import Citizen
+from app.api.groups.models import Group
 from app.api.organizations.models import Organization
 from app.api.payments.models import Payment, PaymentProduct
 from app.api.popup_city.models import PopUpCity
@@ -12,6 +13,7 @@ __all__ = [
     'Application',
     'Attendee',
     'Citizen',
+    'Group',
     'Organization',
     'Payment',
     'PaymentProduct',
