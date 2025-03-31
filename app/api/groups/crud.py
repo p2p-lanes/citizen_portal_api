e0b3e5aaddc25c003aa0fb6abe0b5ce2701488fa
Francisco Viñas <fvinas@fi.uba.ar>
2025-03-31 12:17:36 -0300

    fix: Improve membership check to prevent duplicate citizen additions in CRUDGroup

--- app/api/groups/crud.py
+++ app/api/groups/crud.py
@@ -151,7 +151,7 @@ class CRUDGroup(CRUDBase[models.Group, schemas.GroupBase, schemas.GroupBase]):
             application.group_id = group.id
             db.refresh(application)
 
-        if citizen.id not in group.members:
+        if citizen.id not in [m.id for m in group.members]:
             group.members.append(citizen)
 
         db.commit()
