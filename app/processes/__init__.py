6a3eb60f2f122c1c299e2253b48ca0ca3110eb1e
Francisco Viñas <fvinas@fi.uba.ar>
2025-05-15 16:03:07 -0300

    refactor: consolidate model imports by replacing individual imports with a core models import in processes/__init__.py

--- app/processes/__init__.py
+++ app/processes/__init__.py
@@ -1,6 +1,1 @@
-from app.api.applications.attendees.models import Attendee
-from app.api.applications.models import Application
-from app.api.citizens.models import Citizen
-from app.api.payments.models import Payment, PaymentProduct
-from app.api.popup_city.models import PopUpCity
-from app.api.products.models import Product
+from app.core import models
